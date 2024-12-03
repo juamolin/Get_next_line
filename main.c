6ac3391f47c701551839fb30155e97cf18a497f2
juamolin <juanluismolinaucles@hotmail.com>
2024-12-03 20:36:08 +0100

    I'm testing

--- main.c
+++ main.c
@@ -1,4 +1,15 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 int	main(void)
 {
-	int	
+	int	fd;
+
+	fd = open("file.txt", O_RDWR | O_CREAT);
+
+	printf("fd of file%d\n", fd);
+	write(1, "hello", 5);
+	write(fd, "hello", 5);
+	return 0;
 }
